5d285fa5764a482c45df51946113ff60e001cc6b
Rama El <ramaelmaulanaagung@gmail.com>
2025-02-06 00:11:26 +0700

    minor fix

--- oled_display.py
+++ oled_display.py
@@ -1,14 +1,9 @@
-# Complete project details at https://RandomNerdTutorials.com/micropython-programming-with-esp32-and-esp8266/
-
 from machine import Pin, SoftI2C
 import ssd1306
 from time import sleep
 
 # ESP32 Pin assignment 
 i2c = SoftI2C(scl=Pin(25), sda=Pin(26))
-
-# ESP8266 Pin assignment
-#i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
 
 oled_width = 128
 oled_height = 64
